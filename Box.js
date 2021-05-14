5bd3c3dedd74255a4bf8b6fec60289042de42baa
savanna <savvyeden05@gmail.com>
2021-05-14 18:40:29 +0530

    Added comment lines in sketch.js and box.js

--- Box.js
+++ Box.js
@@ -1,6 +1,6 @@
 class Box extends BaseClass {
   constructor(x, y, width, height){
-    super(x,y,width,height);
+    super(x,y,width,height); //super is a keyword and used to go to parent class
     this.image = loadImage("sprites/wood1.png");
   }
 
